8f82586f063b85f128652e25ee2c4d436640800b
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-01-25 22:44:03 +0500

    update

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 export default function ContactList() {
   return (
     <div>
-      <h1></h1>
-      <h2></h2>
-      <h3></h3>
-      <h4></h4>
+     
       
     </div>
   );
