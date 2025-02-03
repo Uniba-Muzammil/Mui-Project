a7560c522f2d2bf07fecf0323fd8b6d63725ae6a
Uniba-Muzammil <unibamuzammil8@gmail.com>
2025-02-03 23:08:46 +0500

    update

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 export default function AddContact() {
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+     
     </div>
   );
 }
